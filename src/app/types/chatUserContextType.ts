6faddfe6e76d4436af5f7df86fa4bce7384a5ea6
KangWoosung <53413020+KangWoosung@users.noreply.github.com>
2024-05-01 08:30:25 +0900

    Deploy Error fixed 4

--- src/app/types/chatUserContextType.ts
+++ src/app/types/chatUserContextType.ts
@@ -1,12 +1,12 @@
 import { Dispatch, SetStateAction } from "react";
 import { UserType } from "./userType";
-import { ChatsType } from "./chatTypes";
+import { ChatType } from "./chatTypes";
 import { MessageType } from "./messageType";
 
 export type ChatUserContextType = {
   user: UserType | undefined;
   // setUser: Dispatch<SetStateAction<UserType | undefined>>;
-  chats?: ChatsType | undefined;
+  chats?: ChatType | undefined;
   messages?: MessageType | undefined;
   loggedIn: boolean;
   userContext: UserType | undefined;
